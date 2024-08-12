e77766c0d4940b28c5fb2b9faae5920d127b86ff
mwiko <mwikosinyangwe@gmail.com>
2024-08-12 03:04:25 +0100

    ssssssssss

--- website/views.py
+++ website/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
 
 def home(request):
+    return render(request, 'home.html', {})
+
+
+def logout_user(request):
     return render(request, 'home.html', {})
